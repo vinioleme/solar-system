8c0bdcdb35e72d8722ebb7fa076d14e2db73413f
Vinicius Leme <vinileme@gmail.com>
2022-11-25 15:08:41 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    
-    return (<p>Sistema Solar</p>);
+    return (
+      <div>
+        <p>Sistema Solar</p>
+        <Header />
+      </div>
+    );
   }
 }
 
